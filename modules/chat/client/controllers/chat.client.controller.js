e877af28703473ce4d7bc2c1819c85f8790b388f
“YOUR <shnnagai@gmail.com>
2017-10-23 13:19:43 -0400

    fixed the bug on chatUI. fixed the bug on showing the latest message on the bottom

--- modules/chat/client/controllers/chat.client.controller.js
+++ modules/chat/client/controllers/chat.client.controller.js
@@ -5,7 +5,7 @@ angular.module('chat').controller('ChatController', ['$scope', '$location', 'Aut
   function ($scope, $location, Authentication, Socket,chatService) {
     // Create a messages array
     $scope.messages = [];
-    $scope.isSameUser = {bool:false};
+    $scope.UserName = Authentication.user.username;
 
     // If user is not signed in then redirect back home
     if (!Authentication.user) {
@@ -19,25 +19,8 @@ angular.module('chat').controller('ChatController', ['$scope', '$location', 'Aut
 
     // Add an event listener to the 'chatMessage' event
     Socket.on('chatMessage', function (message) {
-      $scope.messages.unshift(message);
+      $scope.messages.push(message);
     });
-
-    //function to check if isUser
-      $scope.checkIfSameUser = function(userID){
-        console.log(userID +" == " + Authentication.user.username + "\n")
-          if(userID === Authentication.user.username){
-              $scope.isSameUser.bool = true;
-         //     console.log("same\n");
-          }else{
-            $scope.isSameUser.bool = false;
-          }
-          console.log("isSameUser is : ");
-          if($scope.isSameUser.bool === true)
-            console.log("true\n");
-            else
-            console.log("false\n");
-      };
-
 
     $scope.retrieveChatAllData = function(){
 
